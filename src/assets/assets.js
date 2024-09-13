5125f6a9feb691bf3e3913a0fcb4624bd8a3a113
Filip <teeqpro@gmail.com>
2024-09-13 13:30:16 +0200

    added mobile navbar support

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,11 +3,13 @@ import SelectedDot from './Selected-Dot.svg';
 import Search from './Search.svg';
 import Notifications from './Notifications.svg';
 import Avatar from './Avatar.png';
+import LogoN from './LogoN.svg';
 
 export const assets = {
     Logo,
     SelectedDot,
     Search,
     Notifications,
-    Avatar
+    Avatar,
+    LogoN
 }
