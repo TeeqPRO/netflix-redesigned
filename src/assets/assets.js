37e06ec3bc0d2bc1d0a377d273a02631953502c8
TeeqPRO <teeqpro@gmail.com>
2024-09-15 14:07:07 +0200

    gived style to Main Movie

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,11 +3,17 @@ import SelectedDot from './Selected-Dot.svg';
 import Search from './Search.svg';
 import Notifications from './Notifications.svg';
 import Avatar from './Avatar.png';
+import Star from './Star.svg';
+import Add from './Add.svg';
+import Play from './Play.svg';
 
 export const assets = {
     Logo,
     SelectedDot,
     Search,
     Notifications,
-    Avatar
+    Avatar,
+    Star,
+    Add,
+    Play
 }
--- src/components/MovieTop/MovieTop.jsx
+++ src/components/MovieTop/MovieTop.jsx
@@ -1,49 +1,52 @@
 import React from 'react';
+import { assets } from '../../assets/assets';
 import './MovieTop.css';
 
 const MovieTop = () => {
     return (
         <div className="MovieTopContainer">
             <div className="Movie-Info">
-                <div className="Movie-Info-Top">
-                    <div className="Movie-Info-Duration">
-                        <p>1h 59m</p>
-                    </div>
-                    <div className="Movie-Info-More">
-                        <div className="Movie-Info-Rating">
-                            <p>8.6</p>
+                <div className="Movie-Info-Placement">
+                    <div className="Movie-Info-Content">
+                        <div className="Movie-Info-Top">
+                            <div className="Movie-Info-Duration">
+                                1h 59m
+                            </div>
+                            <div className="Movie-Info-More">
+                                <div className="Movie-Info-Rating">
+                                    <img src={assets.Star} alt="Star" /> 8.6
+                                </div>
+                                <div className="Movie-Info-Category">
+                                    Horror Science Fiction
+                                </div>
+                                <div className="Movie-Info-Decoration">
+                                    -
+                                </div>
+                                <div className="Movie-Info-Quote">
+                                    No one will hear you in space.
+                                </div>
+                            </div>
                         </div>
-                        <div className="Movie-Info-Category">
-                            <p>Horror, Science Fiction</p>
+                        <div className="Movie-Info-Middle">
+                            <div className="Movie-Info-Title">
+                                Alien: Romulus
+                            </div>
+                            <div className="Movie-Info-Description">
+                                While scavenging the deep ends of a derelict space station, a group of young space colonizers come face to face with the most terrifying life form in the universe.
+                            </div>
                         </div>
-                        <div className="Movie-Info Quote">
-                            <p>No one will hear you in space.</p>
+                        <div className="Movie-Info-Bottom">
+                            <button className='Watch-btn Movie-Info-Button'><img src={assets.Play} alt="Play" /> <p>Watch</p></button>
+                            <button className='Add-list-btn Movie-Info-Button'><img src={assets.Add} alt="Add" /> <p>Add list</p></button>
                         </div>
-                    </div>
-                </div>
-                <div className="Movie-Info-Middle">
-                    <div className="Movie-Info-Title">
-                        <p>Alien: Romulus</p>
-                    </div>
-                    <div className="Movie-Info-Description">
-                        <p>While scavenging the deep ends of a derelict space station, a group of young space colonizers come face to face with the most terrifying life form in the universe.</p>
-                    </div>
-                </div>
-                <div className="Movie-Info-Bottom">
-                    <div className="Movie-Info-Button-Watch">
-                        <button>Watch</button>
-                    </div>
-                    <div className="Movie-Info-Button-Add-List">
-                        <button>Add list</button>
                     </div>
                 </div>
             </div>
+
             <div className="Image">
-                <div className="Movie1">
-                    <div className="Movie1Image" style={{ backgroundImage: "url('https://image.tmdb.org/t/p/original/nu9RRatx8IjEUq6V20HiineymuO.jpg')", backgroundSize: "cover"}}></div>
+                <div className="Movie">
+                    <div className="MovieImage" style={{ backgroundImage: "url('https://image.tmdb.org/t/p/original/nu9RRatx8IjEUq6V20HiineymuO.jpg')", backgroundSize: "cover" }}></div>
                 </div>
-                <div className="Movie2"></div>
-                <div className="Movie3"></div>
             </div>
         </div>
     );
